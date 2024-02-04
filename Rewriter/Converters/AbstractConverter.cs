1405ca90e87c06a6f1aba00f211910288f2efa30
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-04 06:20:43 +0200

    converters chain rebased to abstract class

---
+++ Rewriter/Converters/AbstractConverter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using Rewriter.Configuration;
+
+namespace Rewriter.Converters;
+
+public abstract class AbstractConverter : IObserver<string>
+{
+    protected FileOutputOptions Options;
+
+    protected AbstractConverter(IOptionsMonitor<FileOutputOptions> optionsMonitor)
+    {
+        Options = optionsMonitor.CurrentValue;
+        optionsMonitor.OnChange(option => Options = option);
+    }
+
+    protected abstract void ConvertFile(string fullPath);
+
+    protected static string ConvertPath(string fullOldPath, string newFolderPath) =>
+        newFolderPath + "/" + Path.ChangeExtension(Path.GetFileName(fullOldPath), ".pdf");
+    
+    public void OnCompleted()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnError(Exception error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnNext(string value)
+    {
+        ConvertFile(value);
+    }
+}
--- Rewriter/Workers/IConverter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rewriter.Workers;
-
-public interface IConverter
-{
-    void ConvertFile(string fullPath);
-
-    string ConvertPath(string fullOldPath, string newFolderPath);
-}
