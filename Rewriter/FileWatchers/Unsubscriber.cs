ac34575e3d91ed439e668ad4db85873c5992c7f0
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-05 01:16:31 +0200

    Unsubscriber class as part of observable pattern

---
+++ Rewriter/FileWatchers/Unsubscriber.cs
@@ -0,0 +1,9 @@
+﻿namespace Rewriter.FileWatchers;
+
+public class Unsubscriber<T>(ICollection<IObserver<T>> observers, IObserver<T> observer) : IDisposable
+{
+    public void Dispose()
+    {
+        observers.Remove(observer);
+    }
+}
