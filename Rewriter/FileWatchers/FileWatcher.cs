58aa70c8591a150d9b4c8ced716e02b28f063b9f
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-06 06:39:18 +0200

    fixed file watcher

--- Rewriter/FileWatchers/FileWatcher.cs
+++ Rewriter/FileWatchers/FileWatcher.cs
@@ -1,34 +1,44 @@
-﻿namespace Rewriter.FileWatchers;
+﻿using System.Reflection;
+using Rewriter.Attributes;
+
+namespace Rewriter.FileWatchers;
 
 public class FileWatcher : FileSystemWatcher, IObservable<FileSystemEventArgs>
 {
     private readonly HashSet<IObserver<FileSystemEventArgs>> _converters = [];
-    
+
     public FileWatcher(string path, IEnumerable<string>? filters = null)
     {
         Path = path;
-        NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+        NotifyFilter = NotifyFilters.FileName;
+        
         EnableRaisingEvents = true;
 
         if (filters is null) return;
-        
+
         foreach (var filter in filters)
         {
-            Filters.Add(filter);
+            Filters.Add('*' + filter);
         }
-        
-        Created += (_, args) =>
-        {
-            foreach (var converter in _converters)
-            {
-                converter.OnNext(args);
-            }
-        };
+
+        Created += OnCreated;
     }
-    
+
     public IDisposable Subscribe(IObserver<FileSystemEventArgs> observer)
     {
         _converters.Add(observer);
+        
         return new Unsubscriber<FileSystemEventArgs>(_converters, observer);
+    }
+    
+    private void OnCreated(object sender, FileSystemEventArgs e)
+    {
+        var converter = _converters.FirstOrDefault(conv =>
+            conv.GetType().GetCustomAttribute<ExtensionAttribute>() is { Extensions: { } extensions } &&
+            extensions.Contains(System.IO.Path.GetExtension(e.FullPath)))!;
+        
+        if (File.GetLastWriteTime(e.FullPath) == File.GetLastAccessTime(e.FullPath)) return;
+        
+        converter?.OnNext(e);
     }
 }
