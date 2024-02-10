8b78abc7dffa2675f1bd32d2a395a3b23762b214
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-10 03:56:50 +0200

    fi

--- Rewriter/Extensions/OptionsBuilderExtension.cs
+++ Rewriter/Extensions/OptionsBuilderExtension.cs
@@ -8,7 +8,7 @@ public static class OptionsBuilderExtension
     public static OptionsBuilder<TOptions> ValidateFluentValidation<TOptions>(this OptionsBuilder<TOptions> builder)
         where TOptions : class
     {
-        builder.Services.AddSingleton<IValidateOptions<TOptions>>(serviceProvider =>
+        builder.Services.AddSingleton<IValidateOptions<TOptions>>(serviceProvider => 
             new FluentValidateOptions<TOptions>(serviceProvider, builder.Name));
 
         return builder;
