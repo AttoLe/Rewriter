1f3ce392f30af81be5320b9148c6d13a5d635372
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-10 03:53:59 +0200

    added extension method to check file extensions

---
+++ Rewriter/Extensions/TypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Rewriter.Attributes;
+
+namespace Rewriter.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool IsRightExtensionConverter(this Type type, string extension)
+    {
+        var attribute = type.GetCustomAttribute<ExtensionAttribute>();
+        return attribute is { Extensions: { } extensions } &&
+               extensions.Contains(Path.GetExtension(extension));
+    }
+}
