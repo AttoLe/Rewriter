0e6cbb0a2fc9b5c8836cef21ba54933964daa833
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-10 03:51:17 +0200

    power point converter done

--- Rewriter/Converters/PowerPointConverter.cs
+++ Rewriter/Converters/PowerPointConverter.cs
@@ -2,6 +2,7 @@
 using Microsoft.Office.Core;
 using Rewriter.Attributes;
 using Rewriter.Configuration;
+using Rewriter.Extensions;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 
 namespace Rewriter.Converters;
@@ -10,16 +11,37 @@ namespace Rewriter.Converters;
 public class PowerPointConverter(IOptionsMonitor<FileOutputOptions> optionsMonitor, ILogger<PowerPointConverter> logger)
     : AbstractConverter(optionsMonitor)
 {
+    private PowerPoint.Application _application = new()
+    {
+        FileValidation = MsoFileValidationMode.msoFileValidationSkip,
+    };
+    
     protected override void ConvertFile(string fullPath)
     {
-        var app = new PowerPoint.Application
-        {
-            Visible = MsoTriState.msoTrue
-        };
+        var document = _application.Presentations.Open(fullPath, 0, 0, 0);
+        logger.LogFileConverting(fullPath);
+       
+        document.SaveAs(FileName: ConvertPath(fullPath, Options.FolderPath), 
+            FileFormat: PowerPoint.PpSaveAsFileType.ppSaveAsPDF);
         
-        //logger
-        
-        var document = app.Presentations.Open(fullPath);
-        document.SaveAs(FileName: ConvertPath(fullPath, Options.FolderPath), FileFormat: PowerPoint.PpSaveAsFileType.ppSaveAsPDF);
+        document.Close();
+        File.Delete(fullPath);
+                
+        logger.LogFileConverted(fullPath);
+    }
+
+    public override void OnCompleted()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnError(Exception error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Dispose()
+    {
+        _application.Quit();
     }
 }
