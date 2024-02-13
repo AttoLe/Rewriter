6b979a9eead8ec25944511b30f01e68dfbcd4552
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-13 20:02:23 +0200

    changed validation for FileInputOptions

---
+++ Rewriter/Validation/Validators/FileInputOptionValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Rewriter.Configuration;
+
+namespace Rewriter.Validation.Validators;
+
+public class FileInputOptionsValidator : AbstractValidator<FileInputOptions>
+{
+    public FileInputOptionsValidator()
+    {
+        RuleFor(inputOptions => inputOptions.FileInputList)
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage("Input options is required")
+            .NotEmpty().WithMessage("At least one option is required")
+            .ForEach(x =>
+            {
+                x.ChildRules(inputOption =>
+                {
+                    inputOption.RuleFor(option => option.FolderPaths)
+                        .Cascade(CascadeMode.Stop)
+                        .NotNull().WithMessage("Folder path is required")
+                        .NotEmpty().WithMessage("Folder path is required")
+                        .ForEach(path => path.SetValidator(new PathValidator()));
+
+                    inputOption.RuleFor(option => option.Extensions)
+                        .Cascade(CascadeMode.Stop)
+                        .NotNull().WithMessage("Extensions list is required")
+                        .NotEmpty().WithMessage("At least one extension must is required")
+                        .ForEach(extensionRule =>
+                        {
+                            extensionRule.NotEmpty().WithMessage("Extension must not be empty");
+                            extensionRule.Matches(@"^\.\w+$").WithMessage("Extension must start with a dot");
+                        });
+                });
+            });
+    }
+}
