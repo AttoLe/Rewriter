7c334d1d38a75db13dda19c075112d24b432607c
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-14 04:28:45 +0200

    stable version with new folder configuration

--- Rewriter/Extensions/FileLoggerExtensions.cs
+++ Rewriter/Extensions/FileLoggerExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging.Configuration;
 using Rewriter.Logger;
 
