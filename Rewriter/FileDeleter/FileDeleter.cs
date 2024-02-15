107194bae504b2c0df171f2801abb80edf0c50ee
AttoLe <kucherenko.serhii@lll.kpi.ua>
2024-02-15 02:02:11 +0200

    added deletion functionality as separate abstraction

---
+++ Rewriter/FileDeleter/FileDeleter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using Rewriter.Configuration;
+using Rewriter.Extensions;
+
+namespace Rewriter.FileDeleter;
+
+public class FileDeleter(IOptionsMonitor<FileInputOptions> monitor, ILogger<FileDeleter> logger) : IFileDeleter
+{
+    
+    public bool TryDeleteFile(string path)
+    {
+        if (!monitor.CurrentValue.DeleteOldFile)
+            return true;
+
+        //add recursion deletion through new prop on FileInputOptions
+        
+        return DeleteOneFile(path);
+    }
+
+    private bool DeleteOneFile(string path)
+    {
+        try
+        {
+            File.Delete(path);
+            logger.LogFileDeleted(path);
+            return true;
+        }
+        catch (Exception e)
+        {
+            logger.LogFileDeletionError(path, e.Message);
+            return false;
+        }
+    }
+    
+}
---
+++ Rewriter/FileDeleter/IFileDeleter.cs
@@ -0,0 +1,6 @@
+﻿namespace Rewriter.FileDeleter;
+
+public interface IFileDeleter
+{
+    public bool TryDeleteFile(string path);
+}
